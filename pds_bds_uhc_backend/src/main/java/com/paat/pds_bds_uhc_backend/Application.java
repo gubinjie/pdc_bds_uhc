6cd2580f70fbc62a53727eebb7a43aa4605c5580
unknown <binjie.gu@paat.co>
2021-03-25 18:42:16 +0800

    初始化项目

---
+++ pds_bds_uhc_backend/src/main/java/com/paat/pds_bds_uhc_backend/Application.java
@@ -0,0 +1,17 @@
+package com.paat.pds_bds_uhc_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 主类
+ * @author binjie.gu
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
